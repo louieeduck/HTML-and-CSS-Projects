7b53a8dcd98c94a5c7d631ed689c7cce106fde4b
Evan <evanfretheim@gmail.com>
2021-04-15 18:33:59 -0500

    font and size

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+	<script src="Hello_World.js">
+	</script>
+	<script>
+	function My_First_Function()	{
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+		}
+		</script>
+	</body>
+</html>
